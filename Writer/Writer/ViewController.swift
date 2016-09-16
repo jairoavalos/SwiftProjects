499384ad2d3f05d31433226e5bd9d67b3bdd2db2
Jairo Avalos <jairo.r.avalos@gmail.com>
2016-09-15 22:01:26 -0700

    Adding styling based on toggle

--- Writer/Writer/ViewController.swift
+++ Writer/Writer/ViewController.swift
@@ -14,9 +14,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         textView.becomeFirstResponder()
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.detectIfSettingsChanged), name: UserDefaults.didChangeNotification, object: nil)
     }
 
     // MARK: - Outlets
     @IBOutlet weak var textView: UITextView!
+    
+    let userSettings = UserDefaults.standard
+    
+    // MARK: - Functions
+    func detectIfSettingsChanged() {
+        
+        // if night mode toggle is on
+        // apply night theme
+        // else restore default theme
+        if userSettings.bool(forKey: "night_mode") {
+            view.backgroundColor = UIColor.black
+            UIApplication.shared.statusBarStyle = .lightContent
+            textView.textColor = UIColor.white
+            textView.keyboardAppearance = .dark
+        }
+        else {
+            view.backgroundColor = UIColor.white
+            UIApplication.shared.statusBarStyle = .default
+            textView.textColor = UIColor.black
+            textView.keyboardAppearance = .default
+        }
+    }
 }
 
